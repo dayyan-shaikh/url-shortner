b9363546f958849d3724f59fe1b934525909b5e8
Dayyan Shaikh <dayyanshaikh16@gmail.com>
2024-11-25 19:54:17 +0530

    update

--- app/page.js
+++ app/page.js
@@ -1,37 +1,31 @@
 import Link from "next/link";
+import { Vortex } from "@/components/ui/Vortex";
 
 export default function Home() {
   return (
-    <main>
-       <div className="bg-gray-100 py-20">
-      <div className="container mx-auto flex flex-col md:flex-row items-center px-6 md:px-12">
-        {/* Text Section */}
-        <div className="md:w-1/2 text-center md:text-left mb-6 md:mb-0">
-          <h2 className="text-4xl font-bold text-gray-900 mb-4">
-            Simplify Your Links
-          </h2>
-          <p className="text-gray-700 mb-6">
-            Make your URLs shorter and more shareable with our easy-to-use URL 
-            shortener. Perfect for businesses, marketers, and personal use. 
-            Start shortening your links today and track their performance.
-          </p>
+    <div className="w-full mx-auto  h-screen overflow-hidden">
+      <Vortex
+        backgroundColor="black"
+        rangeY={800}
+        particleCount={500}
+        baseHue={120}
+        className="flex items-center flex-col justify-center px-2 md:px-10  py-4 w-full h-full"
+      >
+        <h2 className="text-white text-2xl md:text-6xl font-bold text-center">
+          Simplify Your Links
+        </h2>
+        <p className="text-white text-sm md:text-2xl max-w-xl mt-6 text-center">
+          Make your URLs shorter and more shareable with our easy-to-use URL
+          shortener. Perfect for businesses, marketers, and personal use.
+        </p>
+        <div className="flex flex-col sm:flex-row items-center gap-4 mt-6">
           <Link href="/shorten">
-          <button className="px-6 py-3 bg-blue-600 text-white rounded-lg hover:bg-blue-700">
+          <button className="px-4 py-2 bg-blue-600 hover:bg-blue-700 transition duration-200 rounded-lg text-white shadow-[0px_2px_0px_0px_#FFFFFF40_inset]">
             Try Now
           </button>
           </Link>
-          
         </div>
-        {/* Image Section */}
-        <div className="md:w-1/2">
-          <img
-            src="https://img.freepik.com/free-vector/flat-hand-drawn-colleagues-working-same-room_23-2148828084.jpg?t=st=1732254674~exp=1732258274~hmac=06fbf452e8f12745783386868e9d56cb83991578004297aa15ea9770087b2344&w=996"
-            alt="URL Shortener Illustration"
-            className="rounded-lg shadow-lg"
-          />
-        </div>
-      </div>
+      </Vortex>
     </div>
-    </main>
   );
 }
