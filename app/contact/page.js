866ea080fc83c18d2f05e5b1effcc9080f92f53a
Dayyan Shaikh <dayyanshaikh16@gmail.com>
2024-11-27 12:43:45 +0530

    added contact page

---
+++ app/contact/page.js
@@ -0,0 +1,63 @@
+"use client"
+import React from "react";
+
+const Contact = () => {
+
+
+  return (
+    <div className="h-[100vh] items-center flex justify-center lg:px-0">
+      <div className="max-w-screen-xl bg-white border shadow sm:rounded-lg flex justify-center flex-1">
+        <div className="flex-1 bg-blue-900 text-center hidden md:flex">
+          <div
+            className="m-12 xl:m-16 w-full bg-contain bg-center bg-no-repeat"
+            style={{
+              backgroundImage: `url(https://www.tailwindtap.com/assets/common/marketing.svg)`,
+            }}
+          ></div>
+        </div>
+        <div className="lg:w-1/2 xl:w-5/12 p-6 sm:p-12">
+          <div className="flex flex-col items-center">
+            <div className="text-center">
+              <h1 className="text-2xl xl:text-4xl font-extrabold text-blue-900">
+                Contact Us
+              </h1>
+            </div>
+            <form className="w-full flex-1 mt-8">
+              <div className="mx-auto max-w-xs flex flex-col gap-4">
+                <input
+                  className="w-full px-5 py-3 rounded-lg font-medium bg-gray-100 border border-gray-200 placeholder-gray-500 text-sm focus:outline-none focus:border-gray-400 focus:bg-white"
+                  type="text"
+                  placeholder="Enter your name"
+                />
+                <input
+                  className="w-full px-5 py-3 rounded-lg font-medium bg-gray-100 border border-gray-200 placeholder-gray-500 text-sm focus:outline-none focus:border-gray-400 focus:bg-white"
+                  type="email"
+                  placeholder="Enter your email"
+                />
+                <div className="relative">
+                  <input
+                    className="w-full px-5 py-3 rounded-lg font-medium bg-gray-100 border border-gray-200 placeholder-gray-500 text-sm focus:outline-none focus:border-gray-400 focus:bg-white"
+                    placeholder="Enter message"
+                  />
+                  <button
+                    type="button"
+                    className="absolute right-3 top-3 text-gray-500 hover:text-gray-700"
+                  >
+                  </button>
+                </div>
+                <button
+                  type="submit"
+                  className="mt-5 tracking-wide font-semibold bg-blue-900 text-gray-100 w-full py-3 rounded-lg hover:bg-indigo-700 transition-all duration-300 ease-in-out flex items-center justify-center focus:shadow-outline focus:outline-none"
+                >
+                  Submit
+                </button>
+              </div>
+            </form>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Contact;
